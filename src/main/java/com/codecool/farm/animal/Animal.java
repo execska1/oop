60be354bad006cc39401e1e1a7fc4a33518c6027
execska1 <execska@gmail.com>
2021-03-24 15:16:38 +0100

    Implement methods in Animal

--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -1,5 +1,15 @@
 package com.codecool.farm.animal;
 
 public abstract class Animal {
+    protected int size;
 
+    public Animal() {
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public abstract void feed();
 }
