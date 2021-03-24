e767ad5650e4bb0639e5ef5475bbafc81a606f6d
execska1 <execska@gmail.com>
2021-03-24 15:28:30 +0100

    Implement methods in Farm

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,38 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
+    private List<Animal> animals;
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Animal> getAnimals() {
+        return this.animals;
+    }
+
+    public void feedAnimals() {
+        animals.forEach(Animal::feed);
+    }
+
+    public void butcher(Butcher butcher) {
+        animals.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty() {
+        return animals.isEmpty();
+    }
+
+    public List<String> getStatus() {
+        List<String> result = new ArrayList<>();
+        animals.forEach(animal -> result.add("There is a " + animal.getSize() + " sized " + animal.toString() +" in the farm."));
+
+        return result;
+    }
 
 }
